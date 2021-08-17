64c8fb1ab725ac4a53e759d67bfca720b368320d
jwade4207 <jwade4207@yahoo.com>
2021-08-16 20:50:02 -0500

    save quote js

---
+++ models/Quote.js
@@ -0,0 +1,63 @@
+const { Schema, model } = require('mongoose');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const QuoteSchema = new Schema({
+    quoteText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+    }
+);
+
+QuoteSchema.virtual('reactionCount').get(function () {
+    return reactions.length;
+});
+
+const Quote = model('Quote', QuoteSchema);
+
+module.exports = Quote
